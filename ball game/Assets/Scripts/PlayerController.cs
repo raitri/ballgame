c758c76e2c59ebf25b594892d73acf1538d36cf8
raitri <rait.rips@tptlive.ee>
2025-09-10 14:01:49 +0300

    Add movement

--- ball game/Assets/Scripts/PlayerController.cs
+++ ball game/Assets/Scripts/PlayerController.cs
@@ -1,13 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed;
-    public Text scoreText;
-    public Text winText;
+    public TMP_Text scoreText;
+    public TMP_Text winText;
     public GameObject gate;
     private Rigidbody rb;
     public int score;
@@ -18,7 +18,7 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         score = 0;
         SetscoreText();
-        winText = "";
+        winText.text = "";
     }
 
     // Update is called once per frame
@@ -28,5 +28,48 @@ public class PlayerController : MonoBehaviour
         float moveVertical = Input.GetAxis("Vertical");
 
         Vector3 movement=new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+        //Restart level
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+
+        //Quit level
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("coin"))
+        {
+            other.gameObject.SetActive(false);
+            score++;
+            SetscoreText();
+            if(score >= 5)
+            {
+                gate.gameObject.SetActive(false);
+            }
+        }
+
+        if (other.gameObject.CompareTag("danger"))
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+    }
+
+    void SetscoreText()
+    {
+        scoreText.text="Score: "+score.ToString();
+
+        if(score >= 10)
+        {
+            winText.text = "You win! Press R to restart or ESC to exit";
+        }
     }
 }
